b8d6c5ebd19197a61c5bf9544cc57a405dc92863
Alin Mihut <alin.mihut@cososys.com>
2021-05-17 18:07:12 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -e
 
 # Builds application files
 docker-compose up -d --build
+docker-compose run --rm php-service composer install
 docker-compose run --rm node-service yarn install
 docker-compose run --rm node-service yarn dev
